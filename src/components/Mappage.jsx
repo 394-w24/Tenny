0eeabded2583a2ae1b86e542fb208c57d843d183
Jin <zhan7075@umn.edu>
2024-02-15 18:36:46 -0600

    map

---
+++ src/components/Mappage.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
+
+
+const MapPage = () => {
+    
+
+    return(
+        <div>
+            <MapContainer
+                center={[40, 83]}
+                zoom={10}
+                style={{ height: "100%", width: "100%" }}
+                
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+
+        
+
+
+
+    )
+}
+
+export default MapPage;
