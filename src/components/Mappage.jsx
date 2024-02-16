39f865c1f7a95ee3da32ce2045b0ee7ebc2f05fc
Kedar Khedekar <titaninum1097@gmail.com>
2024-02-16 14:01:04 -0600

    Added firebase connectivity and displayed profile from DB

--- src/components/Mappage.jsx
+++ src/components/Mappage.jsx
@@ -6,6 +6,9 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import { useDbData } from '../utilities/firebase';
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+
 const customIcon = new L.Icon({
     iconUrl: icon,
     iconRetinaUrl: iconRetina,
@@ -18,14 +21,34 @@ const customIcon = new L.Icon({
 
 const MapPage = () => {
 
-    const proxyData = useDbData("proxies/")
-    // if(proxyData[0] == undefined){
-    //     return (<div></div>)
-    // }
+    const buddies = useDbData("buddies/");
+
+    console.log("buddy list");
+    console.log(buddies);
+
+    if(buddies[0] === undefined) {
+      return (<div></div>)
+    }
+
+    const buddyArray = Object.values(buddies[0])
+    console.log("buddy array")
+    console.log(buddyArray)
+
+    const locations = {}
+
+    Object.entries(buddies[0]).map(([buddyId, buddy]) => {
+      if (locations[buddy.latitude + ":" + buddy.longitude]){
+        console.log(locations)
+        locations[buddy.latitude + ":" + buddy.longitude].push({buddyId, buddy})
+      }else{
+        console.log(locations)
+        locations[buddy.latitude + ":" + buddy.longitude] = [{buddyId, buddy}]
+      }
+    })
     return(
         <div>
             <MapContainer
-                center={[40, -83]}
+                center={[42.048860, -87.679890]}
                 zoom={10}
                 style={{ height: "1000px", width: "100%" }}
 
@@ -34,17 +57,33 @@ const MapPage = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={[40, -83]} icon={customIcon}>
-                    <Popup>
-                        <div className="card" style={{ width: '10rem' }}>
-                            <img src={"https://media.istockphoto.com/id/153011771/photo/cheerful-senior-man.jpg?s=612x612&w=0&k=20&c=BYpISz7fcryegrzbLGVv9pqu7ENRyEKw9QqMxfp4ahw="} className="card-img-top" alt="Trip photo" width="150" height="150"/ >
-                            <p>Joe, Age 62</p>
-                            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                            </div>
-                        </div>
-                    </Popup>
-                </Marker> 
- 
+                {Object.values(locations).map(buddies => (
+                    <Marker position={[buddies[0].buddy.latitude, buddies[0].buddy.longitude]} icon={customIcon}>
+                        <Popup>
+                        {/* <Carousel> */}
+                            {buddies.map(buddy => (
+                                    // <Carousel.Item>
+                                        <div className="card" style={{ width: '18rem' }}>
+                                        {/* <Trip key={trip.tripId} tripId={trip.tripId} trip={trip.trip} simpleView={true}/> */}
+                                            <Card style={{ width: '18rem' }}>
+                                                <Card.Img variant="top" src={buddy.buddy.image_url} />
+                                                <Card.Body>
+                                                    <Card.Title>{buddy.buddy.name}</Card.Title>
+                                                    <Card.Text>
+                                                        Age: {buddy.buddy.age}<br/>
+                                                        Ethinicity: {buddy.buddy.ethinicity}<br/>
+                                                        Rating: {buddy.buddy.rating}/5.0<br/>
+                                                        # Favours: {buddy.buddy.favor_count}
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    // </Carousel.Item>
+                            ))}
+                            {/* </Carousel> */}
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     )
