5258b55f75b5ee9a647e8de9c951385f8f8b79a9
Jin <zhan7075@umn.edu>
2024-02-20 19:58:28 -0600

    working on the banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import servicePage from './components/servicePage';
-import Banner from './components/Banner';
+import Banner from './components/Banner.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
