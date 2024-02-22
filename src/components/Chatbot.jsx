6ab3713eee89a3cabb542c9d45fa8eaa36584c94
Jinx2000 <jinwei.okay@gmail.com>
2024-02-21 20:25:42 -0600

    done with chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect } from 'react';
-import './Chatbot.css';
+import React from 'react';
+import styles from './Chatbot.module.css'
 
 const Chatbot = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js';
-    script.async = true;
-    document.body.appendChild(script);
+  const botpressHtml = {
+    __html: `
+    <div>
+        <div class="${styles.fullPageChatbot}">
+          <iframe
+            style="border: none;"
+            srcdoc="<body><script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
+            <script>
+              window.botpressWebChat.init({
+                  'composerPlaceholder': 'Chat with TennyBot',
+                  'botConversationDescription': 'So do not fear, for TennyBot is with you',
+                  'botName': 'TennyBot',
+                  'botId': 'c5609c09-5a94-4b60-b380-c55c760009b9', // Make sure to use your own botId
+                  'hostUrl': 'https://cdn.botpress.cloud/webchat/v1',
+                  'messagingUrl': 'https://messaging.botpress.cloud',
+                  'clientId': 'c5609c09-5a94-4b60-b380-c55c760009b9', // And clientId
+                  'enableConversationDeletion': true,
+                  'showPoweredBy': true,
+                  'className': 'webchatIframe',
+                  'containerWidth': '100%25',
+                  'layoutWidth': '100%25',
+                  'hideWidget': true,
+                  'showCloseButton': false,
+                  'disableAnimations': true,
+                  'closeOnEscape': false,
+                  'showConversationsButton': false,
+                  'enableTranscriptDownload': false,
+              });
+            window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
+            </script></body>",
+            width="100%"
+            height="100%"
+          ></iframe>
+        </div>
+    </div>
+    `,
+  };
 
-    script.onload = () => {
-      window.botpressWebChat.init({
-        "composerPlaceholder": "Chat with bot",
-        "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
-        "botId": "c5609c09-5a94-4b60-b380-c55c760009b9",
-        "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-        "messagingUrl": "https://messaging.botpress.cloud",
-        "clientId": "c5609c09-5a94-4b60-b380-c55c760009b9",
-        "webhookId": "9201fa33-ae6c-4cab-bde9-87b5268ec0d7",
-        "lazySocket": true,
-        "themeName": "prism",
-        "frontendVersion": "v1",
-        "showPoweredBy": true,
-        "theme": "prism",
-        "themeColor": "#2563eb"
-      });
-
-      // Automatically open the chat window after initialization
-      window.botpressWebChat.sendEvent({ type: 'show' });
-    }
-  }, []);
-
-  return <div id="webchat" />;
+  return <div dangerouslySetInnerHTML={botpressHtml} />;
 };
 
 export default Chatbot;
