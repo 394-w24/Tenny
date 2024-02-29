f9f13722f7321c4d72523d5ef3925144e44c26d0
Kedar Khedekar <titaninum1097@gmail.com>
2024-02-28 20:55:48 -0600

    Get APIKey from firebase db

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -4,12 +4,17 @@ import Message from "./Message";
 import Chatbot from "./Chatbot";
 
 import "./ChatPage.css";
+import { getDbData } from "../utilities/firebase";
 
 const ChatPage = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
+  var apiKey = "";
 
-  console.log("api key: " + import.meta.env.VITE_REACT_APP_OPENAI_API_KEY);
+  getDbData("/").then((data) => {
+    apiKey = data.apiKey;
+  });
+
 
   const handleSubmit = () => {
     const prompt = {
@@ -28,7 +33,7 @@ const ChatPage = () => {
     fetch("https://api.openai.com/v1/assistants", {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+        Authorization: `Bearer ${apiKey}`,
         "Content-Type": "application/json",
         "OpenAI-Beta": "assistants=v1",
         "model": "gpt-3.5-turbo",
@@ -37,25 +42,25 @@ const ChatPage = () => {
     ).then((data) => {
       console.log(data);
       assistant = data.data[0].id
-      console.log(data.data[0].id)
+      // console.log(data.data[0].id)
       // Create a new thread
       fetch("https://api.openai.com/v1/threads", {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+          Authorization: `Bearer ${apiKey}`,
           "Content-Type": "application/json",
           "OpenAI-Beta": "assistants=v1",
           // "model": "gpt-3.5-turbo",
         }
       }).then((data) => data.json()
       ).then((data) => {
-        console.log(data);
+        // console.log(data);
         thread = data.id
         // Add message to the thread
         fetch(`https://api.openai.com/v1/threads/${thread}/messages`, {
           method: "POST",
           headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+            Authorization: `Bearer ${apiKey}`,
             "Content-Type": "application/json",
             "OpenAI-Beta": "assistants=v1",
           },
@@ -65,13 +70,13 @@ const ChatPage = () => {
           }),
         }).then((data) => data.json()
         ).then((data) => {
-            console.log(data)
+            // console.log(data)
           // Run the assistant
-          console.log("assistant id" + assistant)
+          // console.log("assistant id" + assistant)
           fetch(`https://api.openai.com/v1/threads/${thread}/runs`, {
             method: "POST",
             headers: {
-              Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+              Authorization: `Bearer ${apiKey}`,
               "Content-Type": "application/json",
               "OpenAI-Beta": "assistants=v1",
             },
@@ -80,9 +85,9 @@ const ChatPage = () => {
             }),
           }).then((data) => data.json()
           ).then((data) => {
-              console.log(data);
+              // console.log(data);
               runId  = data.id;
-              console.log(runId)
+              // console.log(runId)
               status = data.status;
               // Check assistant status
               console.log(runId);
@@ -92,7 +97,7 @@ const ChatPage = () => {
                 fetch(`https://api.openai.com/v1/threads/${thread}/runs/${runId}`, {
                   method: "GET",
                   headers: {
-                    Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+                    Authorization: `Bearer ${apiKey}`,
                     "Content-Type": "application/json",
                     "OpenAI-Beta": "assistants=v1",
                   },
@@ -108,7 +113,7 @@ const ChatPage = () => {
                       fetch(`https://api.openai.com/v1/threads/${thread}/messages`, {
                         method: "GET",
                         headers: {
-                          Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_OPENAI_API_KEY}`,
+                          Authorization: `Bearer ${apiKey}`,
                           "Content-Type": "application/json",
                           "OpenAI-Beta": "assistants=v1",
                         },
@@ -117,12 +122,12 @@ const ChatPage = () => {
                         // }),
                       }).then((data) => data.json()
                       ).then((data) => {
-                          console.log(data);
+                          // console.log(data);
                           console.log(data.data[0].content[0].text.value);
                           // if (data.data[0].content[0].role === "assistant") {
                             console.log("setting msg")
                             messages1 = [...messages1, {role: "assistant", content: data.data[0].content[0].text.value}];
-                            console.log(messages1)
+                            // console.log(messages1)
                             setMessages(messages1)
                           // }
                           // setStatus(data.status);
